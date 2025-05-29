0a49ad0880cda36f747eb4610ad8e9b43ba806ad
himambashiran <142086470+HimamBashiran@users.noreply.github.com>
2025-05-29 23:31:56 +0700

    Update Penyewaan.java

--- src/gowez/Penyewaan.java
+++ src/gowez/Penyewaan.java
@@ -11,71 +11,33 @@ package gowez;
 import java.time.LocalDateTime;
 
 public class Penyewaan {
-    private int idPenyewaan;
+    private int id;
     private User user;
-    private SepedaListrik sepeda;
+    private Sepeda sepeda;
     private LocalDateTime tanggalMulai;
     private LocalDateTime tanggalSelesai;
     private double biaya;
-
-    public Penyewaan(int idPenyewaan, User user, SepedaListrik sepeda, LocalDateTime tanggalMulai, LocalDateTime tanggalSelesai, double biaya) {
-        this.idPenyewaan = idPenyewaan;
+    
+    public Penyewaan(int id, User user, Sepeda sepeda, LocalDateTime tanggalMulai, LocalDateTime tanggalSelesai, double biaya) {
+        this.id = id;
         this.user = user;
         this.sepeda = sepeda;
         this.tanggalMulai = tanggalMulai;
         this.tanggalSelesai = tanggalSelesai;
         this.biaya = biaya;
     }
-
-    Penyewaan(int i, User user1, Sepeda sepeda, LocalDateTime mulai, LocalDateTime selesai, double biaya) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public int getIdPenyewaan() {
-        return idPenyewaan;
-    }
-
-    public void setIdPenyewaan(int idPenyewaan) {
-        this.idPenyewaan = idPenyewaan;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public SepedaListrik getSepeda() {
-        return sepeda;
-    }
-
-    public void setSepeda(SepedaListrik sepeda) {
-        this.sepeda = sepeda;
-    }
-
-    public LocalDateTime getTanggalMulai() {
-        return tanggalMulai;
-    }
-
-    public void setTanggalMulai(LocalDateTime tanggalMulai) {
-        this.tanggalMulai = tanggalMulai;
-    }
-
-    public LocalDateTime getTanggalSelesai() {
-        return tanggalSelesai;
-    }
-
-    public void setTanggalSelesai(LocalDateTime tanggalSelesai) {
-        this.tanggalSelesai = tanggalSelesai;
-    }
-
-    public double getBiaya() {
-        return biaya;
-    }
-
-    public void setBiaya(double biaya) {
-        this.biaya = biaya;
-    }
+    
+    public int getId() { return id; }
+    public User getUser() { return user; }
+    public Sepeda getSepeda() { return sepeda; }
+    public LocalDateTime getTanggalMulai() { return tanggalMulai; }
+    public LocalDateTime getTanggalSelesai() { return tanggalSelesai; }
+    public double getBiaya() { return biaya; }
+    
+    public void setId(int id) { this.id = id; }
+    public void setUser(User user) { this.user = user; }
+    public void setSepeda(Sepeda sepeda) { this.sepeda = sepeda; }
+    public void setTanggalMulai(LocalDateTime tanggalMulai) { this.tanggalMulai = tanggalMulai; }
+    public void setTanggalSelesai(LocalDateTime tanggalSelesai) { this.tanggalSelesai = tanggalSelesai; }
+    public void setBiaya(double biaya) { this.biaya = biaya; }
 }
